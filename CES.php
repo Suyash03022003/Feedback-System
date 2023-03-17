de033d5123c8bca4627c281fe474fdd2718955ff
Amit Jibhkate <83968832+AmitJibhkate@users.noreply.github.com>
2023-03-17 12:06:38 +0530

    ces css

--- CES.php
+++ CES.php
@@ -11,11 +11,14 @@ $result = mysqli_query($conn, $query);
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/CES.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CSE</title>
 </head>
 <body>
-    <form action="Action.php" method="POST">
+    
+    <form action="Action.php" method="POST" class="ces">
+    <h1>Course Exit Survey :</h1>
     <?php
     $i = 0;
                 while ($row = mysqli_fetch_assoc($result)) {
