b7c08823639fc6ef37a122f5593d9f392e38a274
Suyash Sunil Patalbansi <89703368+Suyash03022003@users.noreply.github.com>
2023-05-02 19:38:19 +0530

    SQL file changed

--- feedbacksystem.sql
+++ feedbacksystem.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2023 at 11:02 AM
+-- Generation Time: May 02, 2023 at 04:06 PM
 -- Server version: 10.4.27-MariaDB
--- PHP Version: 7.4.33
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,9 +30,9 @@ SET time_zone = "+00:00";
 CREATE TABLE `allfeedbackforms` (
   `feedbackId` int(11) NOT NULL,
   `feedbackType` char(10) NOT NULL,
-  `authorId` int(11) NOT NULL,
-  `subjectId` int(11) NOT NULL,
-  `deptId` int(11) NOT NULL,
+  `userId` varchar(15) NOT NULL,
+  `subjectId` char(15) NOT NULL,
+  `deptId` int(10) NOT NULL,
   `year` char(3) NOT NULL,
   `semester` char(4) NOT NULL,
   `section` char(1) DEFAULT NULL,
@@ -58,7 +58,7 @@ CREATE TABLE `department` (
 
 CREATE TABLE `options` (
   `questionId` int(11) NOT NULL,
-  `options` char(10) NOT NULL,
+  `option` char(10) NOT NULL,
   `markAssigned` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -85,7 +85,7 @@ CREATE TABLE `responses` (
   `responseId` int(11) NOT NULL,
   `feedbackId` int(11) NOT NULL,
   `questionId` int(11) NOT NULL,
-  `answer` text NOT NULL
+  `option` char(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -105,10 +105,10 @@ CREATE TABLE `subject` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `user` (
+CREATE TABLE `users` (
   `userId` varchar(14) NOT NULL,
   `fname` char(10) NOT NULL,
   `mname` char(10) NOT NULL,
@@ -131,7 +131,10 @@ CREATE TABLE `user` (
 -- Indexes for table `allfeedbackforms`
 --
 ALTER TABLE `allfeedbackforms`
-  ADD PRIMARY KEY (`feedbackId`);
+  ADD PRIMARY KEY (`feedbackId`),
+  ADD KEY `fk_authorId` (`userId`),
+  ADD KEY `fk_subjectid` (`subjectId`),
+  ADD KEY `fk_departmentId` (`deptId`);
 
 --
 -- Indexes for table `department`
@@ -140,28 +143,39 @@ ALTER TABLE `department`
   ADD PRIMARY KEY (`deptId`);
 
 --
+-- Indexes for table `options`
+--
+ALTER TABLE `options`
+  ADD KEY `fk_questionIdOptions` (`questionId`);
+
+--
 -- Indexes for table `question`
 --
 ALTER TABLE `question`
-  ADD PRIMARY KEY (`questionId`);
+  ADD PRIMARY KEY (`questionId`),
+  ADD KEY `fk_feedbackIdQuestion` (`feedbackId`);
 
 --
 -- Indexes for table `responses`
 --
 ALTER TABLE `responses`
-  ADD PRIMARY KEY (`responseId`);
+  ADD PRIMARY KEY (`responseId`),
+  ADD KEY `fk_feedbackId` (`feedbackId`),
+  ADD KEY `fk_questionId` (`questionId`);
 
 --
 -- Indexes for table `subject`
 --
 ALTER TABLE `subject`
-  ADD PRIMARY KEY (`subjectId`);
+  ADD PRIMARY KEY (`subjectId`),
+  ADD KEY `fk_deptId` (`deptId`);
 
 --
--- Indexes for table `user`
+-- Indexes for table `users`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`userId`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userId`),
+  ADD KEY `foreign_key` (`departmentId`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -190,6 +204,49 @@ ALTER TABLE `question`
 --
 ALTER TABLE `responses`
   MODIFY `responseId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `allfeedbackforms`
+--
+ALTER TABLE `allfeedbackforms`
+  ADD CONSTRAINT `fk_authorId` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`),
+  ADD CONSTRAINT `fk_departmentId` FOREIGN KEY (`deptId`) REFERENCES `department` (`deptId`),
+  ADD CONSTRAINT `fk_subjectid` FOREIGN KEY (`subjectId`) REFERENCES `subject` (`subjectId`);
+
+--
+-- Constraints for table `options`
+--
+ALTER TABLE `options`
+  ADD CONSTRAINT `fk_questionIdOptions` FOREIGN KEY (`questionId`) REFERENCES `question` (`questionId`);
+
+--
+-- Constraints for table `question`
+--
+ALTER TABLE `question`
+  ADD CONSTRAINT `fk_feedbackIdQuestion` FOREIGN KEY (`feedbackId`) REFERENCES `allfeedbackforms` (`feedbackId`);
+
+--
+-- Constraints for table `responses`
+--
+ALTER TABLE `responses`
+  ADD CONSTRAINT `fk_feedbackId` FOREIGN KEY (`feedbackId`) REFERENCES `allfeedbackforms` (`feedbackId`),
+  ADD CONSTRAINT `fk_questionId` FOREIGN KEY (`questionId`) REFERENCES `question` (`questionId`);
+
+--
+-- Constraints for table `subject`
+--
+ALTER TABLE `subject`
+  ADD CONSTRAINT `fk_deptId` FOREIGN KEY (`deptId`) REFERENCES `department` (`deptId`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`departmentId`) REFERENCES `department` (`deptId`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
