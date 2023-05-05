91a5c8c7ca8115ce60d71a5bc692541e51378d98
Suyash Sunil Patalbansi <89703368+Suyash03022003@users.noreply.github.com>
2023-05-05 21:31:24 +0530

    Minor changes

--- delete.php
+++ delete.php
@@ -3,7 +3,10 @@ session_start();
 include('connect.php');
 
 $EMAIL = $_GET['email'];
-
+$query1 = "SELECT * FROM login WHERE EMAIL = '$EMAIL'";
+$result = mysqli_query($conn, $query1);
+$row = mysqli_fetch_array($result);
+$CATEGORY = $row['category'];
 $query = "DELETE FROM login WHERE EMAIL = '$EMAIL'";
 $query_run = mysqli_query($conn, $query); 
 
